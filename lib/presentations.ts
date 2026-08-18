bff1f9cf1baa05cd33a31da926042a72f40e1029
jorgemejia25 <jorgeandresmejiasuchite@gmail.com>
2026-08-18 14:28:18 -0600

    Revert "feat(ipc2): agregar presentación semana 6"
    This reverts commit 4dc312cbe8ec628190483dba830c1bf4a2567a76.

--- lib/presentations.ts
+++ lib/presentations.ts
@@ -86,11 +86,6 @@ export const COURSES: Course[] = [
         file: "Semana 5 - Programacion 2.html",
         slides: 19,
       },
-      {
-        label: "Semana 6 — Proyecto 1: Chapin Warriors",
-        file: "Semana 6 - Programacion 2.html",
-        slides: 27,
-      },
     ],
   },
 ];
